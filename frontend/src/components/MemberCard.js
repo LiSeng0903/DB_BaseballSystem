bae89b7ebb678cfa8fdf36dcf697f1fad3434887
leanne404 <90459334+leanne404@users.noreply.github.com>
2022-12-27 14:09:59 +0800

    relation

--- frontend/src/components/MemberCard.js
+++ frontend/src/components/MemberCard.js
@@ -1,11 +1,11 @@
 import styled from "styled-components"
 import { Card } from 'antd';
-import { Modal } from 'antd';
+import { Modal, Table } from 'antd';
 import { useState, useEffect } from "react";
 
 import { useBaseball } from "../containers/hooks/useBaseball";
 
-
+const { Column } = Table;
 const CardStyle = styled(Card)`
     display: flex;
     flex-direction: column; 
@@ -14,7 +14,7 @@ const CardStyle = styled(Card)`
 `
 const MemberCard = ({member}) => {
 
-    const { get_relatives, get_canPositions } = useBaseball();
+    const { get_relatives, get_canPositions, relatives, canPositions } = useBaseball();
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
@@ -31,14 +31,22 @@ const MemberCard = ({member}) => {
     return (
         <>
             <CardStyle title={member.JerNum?member.JerNum:'manager'} hoverable onClick={() => {
-                showModal()}}>
+                showModal();
+                get_canPositions(member.SID);
+                get_relatives(member.SID);
+                }}>
                 <h3>{member.PName}</h3>
             </ CardStyle>
             <Modal title="詳細資料" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
                 <p>系級：{member.Dept + member.Grade}</p>
                 <p>學號：{member.SID}</p>
                 <p>性別：{member.Sex}</p>
-                <p>守位：{member.position}</p>
+                <p>守位：{canPositions}</p>
+                <Table dataSource={relatives} pagination={false}>
+                    <Column title="姓名" dataIndex="Name" />
+                    <Column title="行動電話" dataIndex="Phone" />
+                    <Column title="關係" dataIndex="Relation" />
+                </Table>
             </Modal>
          </>
     )
